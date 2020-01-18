8d0b8ab3c78bde1ecebe41c1230f9593944d91b7
i474232898 <i474232898@gmail.com>
2020-01-18 19:09:01 +0200

    maximum depth of binary tree

--- Maximum Depth of Binary Tree/index.js
+++ Maximum Depth of Binary Tree/index.js
@@ -1,12 +1,20 @@
-const { serializeTree, deserializeTree } = require('../utils');
+// 104. Maximum Depth of Binary Tree
+const { deserializeTree } = require('../utils');
 /**
  * @param {TreeNode} root
  * @return {number}
  */
 var maxDepth = function (root) {
+  const depth = node => {
+    if (node) {
+      return Math.max(depth(node.left), depth(node.right)) + 1;
+    } else {
+      return 0;
+    }
+  }
 
+  return depth(root);
 };
 
 const root = deserializeTree([3, 9, 20, null, null, 15, 7]);
-const arr = serializeTree(root);
-//[3,9,20,null,null,15,7]
+console.log(maxDepth(root));
